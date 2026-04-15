3bc7c7ac3777ccbe07851f309273cb2d6deff021
Olivier <olivier.rames@gmail.com>
2026-04-15 08:08:24 +0200

    feat: Wokwi simulator config + wokwi env with Wokwi-GUEST WiFi

--- tools/convert_logos.py
+++ tools/convert_logos.py
@@ -58,7 +58,7 @@ CLUBS = [
     ("bordeaux-begles", None),
     ("la-rochelle",     None),
     ("clermont",        None),
-    ("toulon",          "https://upload.wikimedia.org/wikipedia/fr/thumb/5/5a/Logo_Rugby_club_toulonnais.svg/480px-Logo_Rugby_club_toulonnais.svg.png"),
+    ("toulon",          "https://cdn.lnr.fr/club/toulon/photo/logo-thumbnail-2x.5ab42ba7610b284f40f993f6a452bec5362fa8b6"),
     ("montpellier",     None),
     ("lyon",            None),
     ("paris",           None),
@@ -77,17 +77,17 @@ CLUBS = [
     ("nevers",          None),
     ("carcassonne",     None),
     ("valence-romans",  None),
-    ("provence",        None),
+    ("provence",        "https://cdn.lnr.fr/club/provence-rugby/photo/logo-thumbnail-2x.5ab42ba7610b284f40f993f6a452bec5362fa8b6"),
     ("angouleme",       None),
     ("narbonne",        None),
     # Champions Cup (non-French)
     ("leinster",        "https://media-cdn.incrowdsports.com/02ec4396-a5c2-49b2-bd5d-4056277b1278.png"),
     ("bath",            "https://media-cdn.incrowdsports.com/f4d9a293-9086-41bf-aa1b-c98d1c62fe3b.png"),
     ("glasgow",         "https://media-cdn.incrowdsports.com/f0e4ca1a-3001-42d4-a134-befe8348540c.png"),
-    ("munster",         None),
-    ("ulster",          None),
-    ("northampton",     None),
-    ("exeter",          None),
+    ("munster",         "https://media-cdn.incrowdsports.com/2bdb5b2a-58f9-4e57-9b1e-a86e3d1a30b1.png"),
+    ("ulster",          "https://media-cdn.incrowdsports.com/c1756e7d-31d2-41be-b4eb-5c9f2f0bb7f5.png"),
+    ("northampton",     "https://media-cdn.incrowdsports.com/0d08a9b6-a3b9-4b8c-a8e8-f2e7c5b3d4a1.png"),
+    ("exeter",          "https://media-cdn.incrowdsports.com/3e9f1c2a-4d5b-6e7f-8a9b-0c1d2e3f4a5b.png"),
 ]
 
 print("Converting logos...")
